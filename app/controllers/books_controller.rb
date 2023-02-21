2e683ddd91b7e2b38421490824ba94d581e9e79c
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-02-21 16:58:33 +0000

    [ADD]5-25CRUD機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,55 @@
 class BooksController < ApplicationController
+  
   def index
+    @books = Book.all
+    
+    
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Registration successfully"
+      redirect_to books_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
+    
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    
+    if @book.update(book_params)
+     flash[:notice] = "Update successfully"
+     redirect_to book_path(@book.id)
+    else
+     flash.now[:notice] = "Update error"
+     render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Delete successfully"
+    redirect_to '/books'
+  end
+  
+   private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+  
   def index
+    @users = User.all
+    
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to "/users"  
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
